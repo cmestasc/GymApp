e892e1ebe2a15362a938341c4ab6444ac15bace6
Carlos Mestas <carlos93esp@gmail.com>
2022-05-23 22:17:18 +0200

    tarjeta

--- src/app/gym/gym.module.ts
+++ src/app/gym/gym.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { MaterialModule } from '../material/material.module';
 import { EjerciciosComponent } from './pages/ejercicios/ejercicios.component';
+import { TarjetaComponent } from './components/tarjeta/tarjeta.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    EjerciciosComponent
+    EjerciciosComponent,
+    TarjetaComponent
   ],
   imports: [
     CommonModule,
