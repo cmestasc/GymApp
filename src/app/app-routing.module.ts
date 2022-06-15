45b77075ed852106f3e3b596382053c2ad6ec6d4
Carlos Mestas <carlos93esp@gmail.com>
2022-06-15 15:57:34 +0200

    m

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EjerciciosComponent } from './gym/pages/ejercicios/ejercicios.component';
-import { HomeComponent } from './gym/pages/home/home.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import ('./auth/auth.module').then(m => m.AuthModule)
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () => import ('./protected/protected.module').then(m => m.ProtectedModule)
   },
   {
     path: 'gymApp',
